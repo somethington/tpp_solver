b91e9e6905950369ec4c5f31b7c73d1c5e44161d
Gigi Khmaladze <Gigi.Khmaladze@amd.com>
2024-07-06 03:31:25 +0000

    Read functions for tsv and csv files

--- tpp_solver_mt.py
+++ tpp_solver_mt.py
@@ -12,7 +12,15 @@
            (______(((_(((______(@)
            Cooked, Fried, and Prepared by Mr Gugi
 '''
+import pandas as pd
 
+
+# File reading functions
+def read_tsv_file(file_path):
+    return pd.read_csv(file_path, sep='\t')
+
+def read_csv_file(file_path):
+    return pd.read_csv(file_path)
 
 def main():
     pass
